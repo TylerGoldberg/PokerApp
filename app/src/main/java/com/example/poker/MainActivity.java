97aecb20b7f7a7d0919f663c03d075087cd33bf8
chaos <TylerJGoldberg@gmail.com>
2021-11-22 17:33:59 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/poker/MainActivity.java
+++ app/src/main/java/com/example/poker/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.poker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
